2d760037dc66505ed6a7ffe2d3dfd181ffd1e950
TopLayer <vmi1401bsv04@gmail.com>
2023-01-23 03:52:30 +0500

    Update gitignore

--- src/Plugins_2022/zTest/Workspace/Entry/Entry.h
+++ src/Plugins_2022/zTest/Workspace/Entry/Entry.h
@@ -1,30 +1,62 @@
 namespace NAMESPACE
 {
-	void __fastcall Hook_oCNpc_OnDamage_Effects_Start(oCNpc*, void*, oCNpc::oSDamageDescriptor&);
-	Hook<void(__thiscall*)(oCNpc*, oCNpc::oSDamageDescriptor&)> Ivk_oCNpc_OnDamage_Effects_Start(ZENFOR(0x00739A20, 0x007791F0, 0x00785B80, 0x0066EE40), &Hook_oCNpc_OnDamage_Effects_Start, HookMode::Patch);
-	void __fastcall Hook_oCNpc_OnDamage_Effects_Start(oCNpc* npc, void* vtable, oCNpc::oSDamageDescriptor& desc)
+	bool IsValidTransform(oCSpell* spell)
 	{
-		Ivk_oCNpc_OnDamage_Effects_Start(npc, desc);
+		return spell->spellID >= ZENDEF2(26, 47) && spell->spellID <= ZENDEF2(39, 58) && spell->saveNpc && spell->spellCasterNpc;
+	}
 
-		if (desc.pVisualFX || !desc.strVisualFX.IsEmpty() || !desc.fDamageReal)
-			return;
+	void HandleTransform(oCNpc* source, oCNpc* target)
+	{
+		for (zCEventMessage* message : source->GetEM()->messageList)
+			if (!message->IsDeleted())
+				if (oCMsgDamage* damage = message->CastTo<oCMsgDamage>())
+					if (damage->GetSubType() == oCMsgDamage::EV_DAMAGE_PER_FRAME)
+						if (damage->descDamage.pVisualFX)
+						{
+							source->OnDamage_Effects_End(damage->descDamage);
+							damage->Delete();
+						}
 
-		const float damage = desc.aryDamageEffective[oEDamageIndex_Edge] * 0.2f;
+		for (zCEventManager* em : zCEventManager::activeEM)
+			for (zCEventMessage* message : em->messageList)
+				if (oCMsgDamage* damage = message->CastTo<oCMsgDamage>())
+					if (damage->GetSubType() == oCMsgDamage::EV_DAMAGE_PER_FRAME || damage->GetSubType() == oCMsgDamage::EV_DAMAGE_ONCE)
+						if (damage->descDamage.pVobAttacker == source)
+						{
+							damage->descDamage.pVobAttacker = target;
+							damage->descDamage.pNpcAttacker = target;
+						}
 
-		if (damage < 1 || npc->GetProtectionByIndex(oEDamageIndex_Fall) < 0)
-			return;
+		for (zCVob* vob : ogame->GetWorld()->activeVobList)
+			if (oCNpc* npc = vob->CastTo<oCNpc>())
+			{
+				if (npc->GetFocusVob() == source)
+					npc->SetFocusVob(target);
 
-		oCNpc::oSDamageDescriptor dotDesc;
-		ZeroMemory(&dotDesc, sizeof(dotDesc));
-		dotDesc = desc;
-		std::fill_n(dotDesc.aryDamage, oEDamageIndex_MAX, 0ul);
-		dotDesc.enuModeDamage = oEDamageType_Fall;
-		dotDesc.fTimeDuration = 3000.0f;
-		dotDesc.fTimeInterval = 1000.0f;
-		dotDesc.fDamagePerInterval = static_cast<float>(npc->GetProtectionByIndex(oEDamageIndex_Fall)) + damage;
+				if (npc->enemy == source)
+					npc->SetEnemy(target);
+			}
+	}
 
-		oCMsgDamage* const message = new oCMsgDamage{ oCMsgDamage::EV_DAMAGE_PER_FRAME, dotDesc };
-		message->SetHighPriority(true);
-		npc->GetEM()->OnMessage(message, npc);
+	void __fastcall Hook_oCSpell_EndTimedEffect(oCSpell*, void*);
+	Hook<void(__thiscall*)(oCSpell*)> Ivk_oCSpell_EndTimedEffect(ZENFOR(0x0047F330, 0x00489E60, 0x004857F0, 0x00486E10), &Hook_oCSpell_EndTimedEffect, HookMode::Patch);
+	void __fastcall Hook_oCSpell_EndTimedEffect(oCSpell* spell, void* vtable)
+	{
+		Ivk_oCSpell_EndTimedEffect(spell);
+
+		if (IsValidTransform(spell) && spell->CanBeDeleted())
+			HandleTransform(spell->spellCasterNpc, spell->saveNpc);
+	}
+
+	int __fastcall Hook_oCSpell_CastSpecificSpell(oCSpell*, void*);
+	Hook<int(__thiscall*)(oCSpell*)> Ivk_oCSpell_CastSpecificSpell(ZENFOR(0x0047EC70, 0x004896B0, 0x00485340, 0x00486960), &Hook_oCSpell_CastSpecificSpell, HookMode::Patch);
+	int __fastcall Hook_oCSpell_CastSpecificSpell(oCSpell* spell, void* vtable)
+	{
+		int result = Ivk_oCSpell_CastSpecificSpell(spell);
+
+		if (IsValidTransform(spell) && !spell->saveNpc->GetHomeWorld())
+			HandleTransform(spell->saveNpc, spell->spellCasterNpc);
+
+		return result;
 	}
 }
